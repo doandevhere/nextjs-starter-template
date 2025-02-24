5186a4f726ab905ec7f6405071237a9b550153e1
doandevhere <tranduongdoan03122001@gmail.com>
2025-02-24 17:15:03 +0700

    refactor: organize imports statements

--- components/theme/theme-provider.tsx
+++ components/theme/theme-provider.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { themeConfig } from '@/components/theme/theme-config'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import * as React from 'react'
 
 export function ThemeProvider({
   children,
