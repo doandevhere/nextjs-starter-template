f0f1796b8d45f013cde1d851d3a2acab752b9856
doandevhere <tranduongdoan03122001@gmail.com>
2025-02-24 10:33:25 +0700

    refactor: standardize code style with single quotes and semicolon removal

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 /** @type { import("eslint").Linter.Config[] } */
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
-    ignores: ["node_modules", "dist", "build", ".next", "public"],
+    ignores: ['node_modules', 'dist', 'build', '.next', 'public'],
   },
-] 
+]
 
-export default eslintConfig;
+export default eslintConfig
