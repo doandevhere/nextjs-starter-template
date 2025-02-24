d6901f6d1eb2e1f151b26528e2958ee2c86b8f6f
doandevhere <tranduongdoan03122001@gmail.com>
2025-02-24 11:11:25 +0700

    refactor: using `cn` for scalability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+import { ThemeProvider } from '@/components/theme/theme-provider'
+import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import { ThemeProvider } from '@/components/theme/theme-provider'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,7 @@ export default function RootLayout({
       lang='en'
       suppressHydrationWarning
     >
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={cn(geistSans.variable, geistMono.variable, 'antialiased')}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
